f2664bc9bef000301f3282a32468b0bbf6040002
Patrik Pettersson <patrik.pettersson@ericsson.com>
2015-03-03 10:13:50 +0100

    sorted functions

--- main.c
+++ main.c
@@ -4,30 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/* Datastructures */
+
 struct client;
-
-static Display *display;
-static Window root;
-static int screen;
-
-static int width, height;
-
-/* events */
-static void map_request(XEvent *e);
-static void keypress(XEvent *e);
-static void destroy(XEvent *e);
-void configurerequest(XEvent *e);
-/* Callbacks */
-static void run_terminal(void);
-
-
-static void (*events[LASTEvent])(XEvent *e) = {
-	[KeyPress] = keypress,
-	[MapRequest] = map_request,
-	[DestroyNotify] = destroy,
-	[ConfigureRequest] = configurerequest
-};
-
 struct key {
 	unsigned int mod;
 	KeySym keysym;
@@ -45,43 +24,44 @@ struct column {
 	struct client *client_list;
 };
 
+
+/* Prototypes */
+static void configurerequest(XEvent *e);
+static void destroy(XEvent *e);
+static void keypress(XEvent *e);
+static void map_request(XEvent *e);
+static void run_terminal(void);
+static void tile(void);
+/* Globals */
+static struct column column = { 0, 0 };
+static Display *display;
+static Window root;
+static int screen;
+static int width, height;
+
 static struct key keys[] = {
 	{Mod1Mask, XK_Return, run_terminal},
 };
 
-static struct column column = { 0, 0 };
+static void (*events[LASTEvent])(XEvent *e) = {
+	[KeyPress] = keypress,
+	[MapRequest] = map_request,
+	[DestroyNotify] = destroy,
+	[ConfigureRequest] = configurerequest
+};
 
-static void keypress(XEvent *e)
-{
-	int i;
-	XKeyEvent ke = e->xkey;
-	int keysyms_per_keycode_return;
-	KeySym *keysym = XGetKeyboardMapping(display,
-					     ke.keycode,
-	                                     1,
-                                             &keysyms_per_keycode_return);
-	for (i = 0; i < sizeof(keys)/sizeof(struct key); i++)
-	{
-		if (keys[i].keysym == keysym[0] && keys[i].mod == ke.state)
-		{
-			keys[i].callback();
-			break;
-		}
-	}
-}
-
-void tile()
-{
-	int y = 0;
-	struct client *tmp;
-	if (!column.nclients)
-		return;
-	for (tmp=column.client_list; tmp; tmp=tmp->next)
-	{
-		XMoveResizeWindow(display, tmp->window, 0,y, width, height/column.nclients);
-		y += height/column.nclients;
-	}
-
+void configurerequest(XEvent *e) {
+	// Paste from DWM, thx again \o/
+	XConfigureRequestEvent *ev = &e->xconfigurerequest;
+	XWindowChanges wc;
+	wc.x = ev->x;
+	wc.y = ev->y;
+	wc.width = ev->width;
+	wc.height = ev->height;
+	wc.border_width = ev->border_width;
+	wc.sibling = ev->above;
+	wc.stack_mode = ev->detail;
+	XConfigureWindow(display, ev->window, ev->value_mask, &wc);
 }
 
 static void destroy(XEvent *e)
@@ -108,6 +88,25 @@ static void destroy(XEvent *e)
 		}
 	}
 	tile();
+}
+
+static void keypress(XEvent *e)
+{
+	int i;
+	XKeyEvent ke = e->xkey;
+	int keysyms_per_keycode_return;
+	KeySym *keysym = XGetKeyboardMapping(display,
+					     ke.keycode,
+	                                     1,
+                                             &keysyms_per_keycode_return);
+	for (i = 0; i < sizeof(keys)/sizeof(struct key); i++)
+	{
+		if (keys[i].keysym == keysym[0] && keys[i].mod == ke.state)
+		{
+			keys[i].callback();
+			break;
+		}
+	}
 }
 
 static void map_request(XEvent *e)
@@ -139,19 +138,6 @@ static void map_request(XEvent *e)
 	tile();
 }
 
-void configurerequest(XEvent *e) {
-	// Paste from DWM, thx again \o/
-	XConfigureRequestEvent *ev = &e->xconfigurerequest;
-	XWindowChanges wc;
-	wc.x = ev->x;
-	wc.y = ev->y;
-	wc.width = ev->width;
-	wc.height = ev->height;
-	wc.border_width = ev->border_width;
-	wc.sibling = ev->above;
-	wc.stack_mode = ev->detail;
-	XConfigureWindow(display, ev->window, ev->value_mask, &wc);
-}
 static void run_terminal(void)
 {
 	int ret;
@@ -160,6 +146,20 @@ static void run_terminal(void)
 		ret = system("xterm");
 		exit(ret);
 	}
+}
+
+static void tile(void)
+{
+	int y = 0;
+	struct client *tmp;
+	if (!column.nclients)
+		return;
+	for (tmp=column.client_list; tmp; tmp=tmp->next)
+	{
+		XMoveResizeWindow(display, tmp->window, 0,y, width, height/column.nclients);
+		y += height/column.nclients;
+	}
+
 }
 
 int main(int argc, char **Argv)
